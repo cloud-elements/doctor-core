d43e9fd561b0121af4ebbb8f8f2f91ed79bf92ba
Bayyaram Mukund <mukund@cloud-elements.com>
2021-02-04 09:51:00 +0530

    Minor bug fixes

--- src/core/commonResources/saveObjectDefinitions.js
+++
@@ -1,6 +0,0 @@
-const {identity, useWith} = require('ramda');
-const http = require('../../utils/http');
-const save = require('../../utils/saveToFile');
-
-// (parms, env)
-module.exports = useWith(save, [identity, http.get('organizations/objects/definitions')]);
--- src/utils/http.js
+++ src/utils/http.js
@@ -5,11 +5,11 @@ const buildURL = (baseUrl, endpoint) => `${baseUrl}/elements/api-v2/${endpoint}`
 
 // TODO: Will be replacing request-promise with axios soon
 module.exports = {
-  get: curry(async (endpoint, qs = {}, account) => {
+  get: curry(async (path, qs = {}, account) => {
     const options = {
       method: 'GET',
       headers: {Authorization: account.authorization},
-      url: buildURL(account.baseUrl, endpoint),
+      url: buildURL(account.baseUrl, path),
       qs,
       json: true,
       strictSSL: false,
@@ -23,11 +23,11 @@ module.exports = {
       throw err;
     }
   }),
-  post: curry(async (endpoint, body, account) => {
+  post: curry(async (path, body, account) => {
     const options = {
       method: 'POST',
       headers: {Authorization: account.authorization},
-      url: buildURL(account.baseUrl, endpoint),
+      url: buildURL(account.baseUrl, path),
       body,
       json: true,
       strictSSL: false,
@@ -39,11 +39,11 @@ module.exports = {
       throw err;
     }
   }),
-  update: curry(async (endpoint, body, account) => {
+  update: curry(async (path, body, account) => {
     const options = {
       method: 'PUT',
       headers: {Authorization: account.authorization},
-      url: buildURL(account.baseUrl, endpoint),
+      url: buildURL(account.baseUrl, path),
       body,
       json: true,
       strictSSL: false,
@@ -55,11 +55,11 @@ module.exports = {
       throw err;
     }
   }),
-  delete: async (endpoint, qs = {}, account) => {
+  delete: async (path, qs = {}, account) => {
     const options = {
       method: 'DELETE',
       headers: {Authorization: account.authorization},
-      url: buildURL(account.baseUrl, endpoint),
+      url: buildURL(account.baseUrl, path),
       qs,
       json: true,
       strictSSL: false,
